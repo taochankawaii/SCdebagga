180fa4bb521cd115731f1907df27e6fbb3b53d28
g15920ka <g15920ka@s30525.tsuda.ac.jp>
2017-05-08 13:54:34 +0900

    テストケースがつきました

---
+++ snake-camel/test/snakecamel/Sc.java
@@ -0,0 +1,42 @@
+package snakecamel;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Sc {
+
+	/*@Test
+	public void test() {
+		fail("Not yet implemented");
+	}*/
+	@Test
+	public void snake() {
+	SnakeCamelUtil scu = new SnakeCamelUtil();
+	String i = "TAO";
+	String j = scu.snakeToCamelcase("t_a_o");
+	assertThat(j,is(i));
+	}
+	@Test
+	public void camel() {
+	SnakeCamelUtil scu = new SnakeCamelUtil();
+	String i = "t_a_o";
+	String j = scu.camelToSnakecase("TAO");
+	assertThat(j,is(i));
+	}
+	@Test
+	public void capi() {
+	SnakeCamelUtil scu = new SnakeCamelUtil();
+	String i = "Tao";
+	String j = scu.capitalize("Tao");
+	assertThat(j,is(i));
+	}
+	@Test
+	public void uncapi() {
+	SnakeCamelUtil scu = new SnakeCamelUtil();
+	String i = "Tao";
+	String j = scu.capitalize("Tao");
+	assertThat(j,is(i));
+	}
+}
